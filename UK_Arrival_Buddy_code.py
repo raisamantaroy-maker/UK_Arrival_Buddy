1e2b986f6a73e35e00746f0763d023dba4f31c75
raisamantaroy-maker <raisamantaroy@gmail.com>
2026-03-20 09:35:02 +0000

    Update UK_Arrival_Buddy_code.py

--- UK_Arrival_Buddy_code.py
+++ UK_Arrival_Buddy_code.py
@@ -4,11 +4,11 @@ window=tk.Tk()
 window.title("UK Arrival Buddy")
 window.geometry("400x700")
 window.configure(bg="#0b2a4a")
-#function to clear window
+#function to clear window and open new window
 def clear():
     for widget in window.winfo_children():
         widget.destroy()
-#2nd page
+#2nd page login
 def show_login():
     clear()
     tk.Label(window,text="Login Here!",fg="white",bg="#0b2a4a",font=("Times",26,"italic")).pack(pady=80)
@@ -17,7 +17,7 @@ def show_login():
     tk.Label(window,text="Password",fg="white",bg="#0b2a4a",font=("Times",26,"italic")).pack()
     tk.Entry(window,width=30,show="*").pack(pady=5)
     tk.Button(window,text="Login",font=("Times",16,"italic"),bg="#d9d9d9",fg="white",width=10,command=show_home).pack(pady=60)
-#3rd page
+#3rd page home
 def show_home():
     clear()
     tk.Label(window,text="HOME",fg="white",bg="#0b2a4a",font=("Times",26,"italic")).pack(pady=40)
@@ -29,7 +29,7 @@ def show_home():
     tk.Button(window,text="Part-time Jobs",font=("Times",16,"italic"),bg="#d9d9d9",width=25).pack(pady=5)
     tk.Button(window,text="Transport", font=("Times",16,"italic"),bg="#d9d9d9",width=25).pack(pady=5)
     tk.Button(window,text="Emergency Contacts",font=("Times",16,"italic"),bg="#d9d9d9",width=25).pack(pady=5)
-#4th page
+#4th page visa verification
 def visa_page():
     clear()
     tk.Label(window,text="Visa Verification",fg="white",bg="#0b2a4a",font=("Times",22,"italic")).pack(pady=30)
@@ -44,12 +44,12 @@ def visa_page():
     visa.pack(pady=5)
     tk.Button(window,text="Submit",font=("Times",12,"italic"),command=verification_submitted).pack(pady=20)
     tk.Button(window,text="Back",font=("Times",12,"italic"),command=show_home).pack()
-#5th page
+#5th page submitted
 def verification_submitted():
     clear()
     tk.Label(window,text="Verification Submitted",fg="white",bg="#0b2a4a",font=("Times",22,"italic")).pack(pady=200)
     tk.Button(window,text="Back to Home",font=("Times",12,"italic"),command=show_home).pack()    
-#1st page
+#1st page get started
 tk.Label(window,text="UK Arrival Buddy",fg="white",bg="#0b2a4a",font=("Times",26,"italic")).pack(pady=250)
 tk.Button(window,text="Get Started",bg="#d9d9d9",width=15,command=show_login,font=("Times",26,"italic")).pack()
 #run program
